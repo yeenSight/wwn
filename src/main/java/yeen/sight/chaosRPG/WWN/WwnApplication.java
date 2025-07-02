6baf9ebcba022aeca4a8db8a193c0ac577b2afb0
kelkun <yeen.sight@gmail.com>
2025-07-02 12:02:10 +0200

    Init folders

---
+++ src/main/java/yeen/sight/chaosRPG/WWN/WwnApplication.java
@@ -0,0 +1,13 @@
+package yeen.sight.chaosRPG.WWN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yeen/sight/chaosRPG/WWN/WwnApplicationTests.java
@@ -0,0 +1,13 @@
+package yeen.sight.chaosRPG.WWN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
